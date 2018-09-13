123825606665dd274dee42b2ce21e136263aaec2
Boris <b.stapel@zaanstad.nl>
2018-09-13 10:49:54 +0200

    add to dplyr and ggplot2 script

--- werkmap/dplyr.R
+++ werkmap/dplyr.R
@@ -2,8 +2,9 @@
 library("tidyverse") # tidyverse gaan we vandaag het meest gebruiken. 
                      # Hier zitten alle packages in die je nodig hebt
 
-install.packages("gapminder") ##installeer gapminder package 
-df <- gapminder::gapminder ##laad de gapminderdata in r
+#install.packages("gapminder") ##installeer gapminder package 
+df <- gapminder::gapminder %>% 
+  mutate(pop = as.numeric(pop)) ##laad de gapminderdata in r
 
 ## 1. type 'df' in je console om de data te bekijken 
 ## 2. de View() functie is hier ook erg handig voor 
@@ -59,6 +60,87 @@ filter(...)
 
 ###OPDRACHT 3.2
 ###Maak een set met landen in Asia die in 2002 een populatie hadden die kleiner was dan 15000000
+
+
+
+
+#COUNT
+###tel hoeveel rijen de dataset heeft!
+count(df)
+
+###Op dit moment is deze functie niet zo spannend. Zometeen gaan we functies combineren
+###en wordt het een stuk interessanter!
+
+
+
+
+
+
+
+#GROUP_BY en SUMMARISE
+###groepeer je data. Deze handeling zal op zichzelf niet veel met je data doen. 
+###Je wanneer je de data echter aanroept in je console zal je zien dat er bij aan wordt 
+###gegeven wat de groepen zijn. 
+
+df_groep <- group_by(df, country)
+
+###Nu het dataframe gegroepeerd is kan je met de summarise functie
+###dezelfde functies als hierboven toepassen, maar nu worden de functies 
+###per groep toegepast! Wat betekent dat? 
+summarise(df_groep, som = sum(pop))
+
+### als je regel 85 en 90 hebt begrepen kan je nu je eigen tabellen gaan maken. 
+
+###OPDRACHT 4.1
+###Bereken per continent de gemiddelde levensverwachting
+#HINT: je kan hier regel 85 en 90 voor overnemen. Je hoeft alleen country en de 
+#sum functie aan te passen!
+
+
+
+###OPDRACHT 4.2
+### Maak een tabel waarbij je kan zien wat de gemiddelde populatiegrootte is per jaar!
+##HINT: groepeer per jaar en gebruik de mean() functie. 
+
+
+
+
+###DE PIPE ( %>% )
+#De pipe operator is een krachtig gereedschap die met de tidyverse meekomt. 
+# 1. We gaan de pipe altijd gebruiken aan het einde van een regel code
+# 2. De pipe geeft de uitkomst van de huidige regel door aan de volgende regel
+
+###VOORBEELD:
+2 + 2 %>% 
+  + 2 %>% 
+  - 3
+
+#je kan het antwoord ook opslaan zodat je het later kan gebruiken voor een visualisatie
+x <- 2 + 2 %>% 
+  + 2 %>% 
+  - 3
+
+#vaak willen we verschillende bewerkingen uitvoeren op een set. De pipe maakt dit
+#heel gemakkelijk!
+df %>% #gebruik df
+  filter(year == 2002) %>% #filter df op 2002
+  group_by(continent) %>%  #groepeer de gefilterde df op continent
+  summarise(avg_pop = mean(pop)) #bereken voor elk continent in 2002 de gemiddelde pop
+
+
+#OPDRACHT 5.1
+#ook dit wil je misschien visualiseren. Neem hieronder de bovenstaande code over.
+#Wijs de nieuwe tabel toe aan avg_pop_2002
+#hint: gebruik regel 119 als voorbeeld voor het toewijzen. 
+  
+
+
+
+
+
+
+
+
 
 
 
---
+++ werkmap/ggplot2.R
@@ -0,0 +1,82 @@
+#GGPLOT2
+
+#DATA VISUALISEREN MET GGPLOT2 UIT DE TIDYVERSE!
+library("tidyverse") # tidyverse gaan we vandaag het meest gebruiken. 
+# Hier zitten alle packages in die je nodig hebt
+options(scipen = 999)
+
+#install.packages("gapminder") ##gapminder hoeven we niet te installeren. Dat heb je al gedaan. 
+df <- gapminder::gapminder %>% ##laad de gapminderdata in r
+  mutate(pop = as.numeric(pop)) 
+
+## 1. type 'df' in je console om de data te bekijken 
+## 2. de View() functie is hier ook erg handig voor 
+View(df)
+
+
+
+###Zojuist heb je met dplyr geleerd hoe je tabellen kan maken met informatie zoals 
+###jij die wil zien!
+###We gaan verder met het visualiseren van de tabel die je aan het einde van 
+###het dplyr script hebt gemaakt!
+
+avg_pop_2002 <- df %>% #gebruik df
+  filter(year == 2002) %>% #filter df op 2002
+  group_by(continent) %>%  #groepeer de gefilterde df op continent
+  summarise(avg_pop = mean(pop)) #bereken voor elk continent in 2002 de gemiddelde pop
+
+###Met ggplot2 gaan we deze tabel visueel maken. Bij ggplot hoef je vandaag maar 2 dingen
+###te doen. 
+### 1. Aangeven welke data je gebruikt en hoe je deze gebruikt
+### 2. Een plot type uitkiezen. 
+
+### In de praktijk krijg je dan code die er als volgt uit ziet: 
+ggplot(data = avg_pop_2002, aes(x = continent, y = avg_pop)) +
+  geom_col()
+
+#OPDRACHT1.1
+###je kan echter nog vanalles veranderen aan deze plot! Gebruik de cheat sheet om 
+### 1. de kleur van de staven te veranderen, 
+### 2. Een titel aan de plot te geven 
+### 3. De plot te kantelen
+### 4. Gebruik stackoverflow om er achter te komen hoe je de balken van groot naar klein zet. 
+
+
+
+###COMBINEREN DPLYR EN GGPLOT
+#Nu gaan we een nieuwe set maken en deze visualiseren voor je opdracht gever. 
+#Je opdrachtgever, Jan, wil weten of er over de jaren heen een toename is van zowel 
+#levensverwachting als populatie. 
+
+
+#OPDRACHT 2.1
+#maak een tabel met daarin per jaar: de gemiddelde levensverwachting en de totale populatie
+#van alle landen
+#HINT: gebruikt group_by, summarise, mean en sum!
+
+
+
+#OPDRACHT 2.2
+#Maak 2 grafieken waarin je het verloop van levensverwachting en totale populatie laat zien!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
